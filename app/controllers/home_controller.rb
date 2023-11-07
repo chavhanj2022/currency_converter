50b0e895cd553773f020852efbe4bfc26430cfa3
Jitendra Chavhan <chavhan.jitendra@motwane.com>
2023-11-07 17:32:48 +0530

    added routes for exchange

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,21 @@
 class HomeController < ApplicationController
   def index
+    url = URI.parse('http://api.currencylayer.com/live?access_key=35cd5d2f3a7d8d5bbfa9f5f1cf991f79&source=USD')
+    http = Net::HTTP.new(url.host, url.port)
+    request = Net::HTTP::Get.new(url.request_uri)
+    response = http.request(request)
+
+    @live = if response.code.to_i == 200
+              response.body
+            else
+              { "success": false }
+            end
+  rescue StandardError => e
+    error = "An error occurred: #{e.message}"
+    puts error
+  end
+
+  def fetch_exchange_rates
+    puts params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  # get 'converter', to: 'converter#fetch_exchange_rates'
+  post 'converted', to: 'home#fetch_exchange_rates'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
